3becf11f5407a1d2797aed6687cc988f4e4dbe53
Will Comfort <comfortwb21@gmail.com>
2019-09-03 19:59:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,9 @@ end
 
 def monopoly_with_second_tier
  	monopoly = {
-	  :railroads => {:pieces => 4},
-	                :rent_in_dollars => 
+	  :railroads => {:pieces => 4,
+	                 :rent_in_dollars =>
+	  },
 	}
 end
 
