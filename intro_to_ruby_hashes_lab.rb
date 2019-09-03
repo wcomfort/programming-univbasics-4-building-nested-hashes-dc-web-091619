6fe81a92e89a492e5bf0abfb323a76e73d693037
Will Comfort <comfortwb21@gmail.com>
2019-09-03 20:15:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,12 @@ def monopoly_with_third_tier
 	    :three_pieces_owned => 75,
 	    :four_pieces_owned => 100
 	   },
-	   :names => {}
+	   :names => {
+	     :reading_railroad =>{},
+	     :pennsyvania_railroad => {},
+	     :b_and_o_railroad => {},
+	     :shortline_railroad => {},
+	   },
 	  },
 	}
 end
