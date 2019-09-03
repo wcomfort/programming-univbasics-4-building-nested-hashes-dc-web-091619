df95d204d11a4e566d2990394a2c69344743e7cb
Will Comfort <comfortwb21@gmail.com>
2019-09-03 20:04:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
 	  :railroads => 
 	  {:pieces => 4,
 	   :rent_in_dollars => 
-	   {:one_piece_,
+	   {:one_piece,
 	    :two_piece,
 	    :three_piece,
 	    :four_piece
