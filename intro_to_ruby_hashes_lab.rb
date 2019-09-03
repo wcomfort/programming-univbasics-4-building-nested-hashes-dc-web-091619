b1759b65710ad95b8fb45927570f4b4a035e945d
Will Comfort <comfortwb21@gmail.com>
2019-09-03 20:16:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ def monopoly_with_fourth_tier
 	   :rent_in_dollars => 
 	   {:one_piece_owned => 25,
 	    :two_pieces_owned => 50,
-	    :three_pieces_owned => 75,
+	    :three_pieces_owned => 100,
 	    :four_pieces_owned => 100
 	   },
 	   :names => {
